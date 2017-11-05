db227dd952e82631e64335aeb929fb3b8105ce46
puolival <tuomas.puolivali@helsinki.fi>
2017-11-05 11:08:36 +0200

    Test with non-significant p-values.

--- tests/test_fwer.py
+++ tests/test_fwer.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.append('../')
 
+import numpy as np
+
 import unittest
 
 from data import neuhaus
@@ -12,13 +14,21 @@ class TestFWER(unittest.TestCase):
         """Test the Bonferroni procedure using the Neuhaus et al. data."""
         pvals = neuhaus()
         significant = bonferroni(pvals, alpha=0.05)
-        """The first three tests should be significant after the 
+        """The first three tests should be significant after the
         correction."""
         for i in range(0, 3):
             self.assertTrue(significant[i])
-        """The other tests should not be significant after the 
+        """The other tests should not be significant after the
         correction."""
         for i in range(3, 15):
+            self.assertFalse(significant[i])
+
+    def test_bonferroni_nonsig(self):
+        """Test the Bonferroni procedure using p-values in the range
+        [0.05, 1)."""
+        pvals = np.asarray([0.05, 0.3, 0.5, 1.0, 0.05])
+        significant = bonferroni(pvals, alpha=0.05)
+        for i in range(0, len(pvals)):
             self.assertFalse(significant[i])
 
 if __name__ == '__main__':
