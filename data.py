60ce78af2a60e361ff5a9423d8632e08dc3bfc6f
Puoliväli Tuomas B <puolival@dx7-h916-01.biocenter.helsinki.fi>
2017-12-29 11:42:16 +0200

    Option for returning the p-values in random order.

--- data.py
+++ data.py
@@ -3,24 +3,35 @@
 
 References:
 
-[1] Neuhaus KL, von Essen R, Tebbe U, Vogt A, Roth M, Riess M, Niederer W, 
-Forycki F, Wirtzfeld A, Maeurer W, Limbourg P, Merx W, Haerten K (1992): 
-Improved thrombolysis in acute myocardial infarction with front-loaded 
-administration of alteplase: Results of the rt-PA-APSAC patency study 
-(TAPS). Journal of the American College of Cardiology 19(5):385-391.
+[1] Neuhaus KL, von Essen R, Tebbe U, Vogt A, Roth M, Riess M, Niederer W,
+    Forycki F, Wirtzfeld A, Maeurer W, Limbourg P, Merx W, Haerten K (1992):
+    Improved thrombolysis in acute myocardial infarction with front-loaded
+    administration of alteplase: Results of the rt-PA-APSAC patency study
+    (TAPS). Journal of the American College of Cardiology 19(5):385-391.
 
 [2] Benjamini Y, Hochberg Y (1995): Controlling the false discovery rate:
-A practical and powerful approach to multiple testing. Journal of Royal
-Statistical Society. Series B (Methodological): 57(1):289-300.
+    A practical and powerful approach to multiple testing. Journal of Royal
+    Statistical Society. Series B (Methodological): 57(1):289-300.
 
 Last modified: 12th October 2017.
 """
 
 import numpy as np
+from numpy.random import permutation
 
-def neuhaus():
-    """Function that returns the Neuhaus et al. data that was re-analyzed in 
-    the classic Benjamini & Hochberg (1995) FDR paper."""
-    return np.array([0.0001, 0.0004, 0.0019, 0.0095, 0.0201, 0.0278, 0.0298, 
-                     0.0344, 0.0459, 0.3240, 0.4262, 0.5719, 0.6528, 0.7590, 
-                     1.000], dtype='float')
+def neuhaus(permute=False):
+    """Function that returns the Neuhaus et al. data that was re-analyzed in
+    the classic Benjamini & Hochberg (1995) FDR paper.
+
+    Input arguments:
+    permute - If true, the p-values are returned in random order. If false,
+              the p-values are returned in ascending order.
+    """
+    pvals = np.array([0.0001, 0.0004, 0.0019, 0.0095, 0.0201, 0.0278, 0.0298,
+                      0.0344, 0.0459, 0.3240, 0.4262, 0.5719, 0.6528, 0.7590,
+                      1.000], dtype='float')
+    if (permute):
+        m = len(pvals)
+        pvals = pvals[permutation(m)]
+    return pvals
+
