67197c2968ec42e8cd16a6c9fb1c51c7f5eb0b20
Puoliväli Tuomas B <puolival@dx7-h916-01.biocenter.helsinki.fi>
2017-12-29 13:50:48 +0200

    Setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='multipy',
+      version='0.1',
+      description='Multiple hypothesis testing in Python',
+      url='https://puolival.github.io/multipy/',
+      author='Tuomas Puoliväli',
+      author_email='tuomas.puolivali@helsinki.fi',
+      license='Revised 3-clause BSD',
+      packages=['multipy'],
+      zip_safe=False)
+
+
