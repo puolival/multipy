97d4fafae441ab3565e73b8d0e9d2fea91c98d98
Puoliväli Tuomas B <puolival@dx7-h916-01.biocenter.helsinki.fi>
2018-01-03 12:00:16 +0200

    Reorganized the code.

--- multipy/fdr.py
+++ multipy/fdr.py
@@ -3,27 +3,27 @@
 
 The procedures implemented here are:
 
-abh - The adaptive linear step-up procedure, which is defined on page 494 
+abh - The adaptive linear step-up procedure, which is defined on page 494
       in [1] and originally in [2]
 lsu - The non-adaptive one-stage linear step-up procedure
 orc - The 'Oracle' procedure, which is defined on pages 493 and 500 in [1]
-tst - The two-stage linear step-up procedure, which is defined on page 495 
+tst - The two-stage linear step-up procedure, which is defined on page 495
       in reference [1]
 
 References:
 
-[1] Benjamini Y, Krieger AM, Yekutieli D (2006): Adaptive linear step-up 
+[1] Benjamini Y, Krieger AM, Yekutieli D (2006): Adaptive linear step-up
 procedures that control the false discovery rate. Biometrika 93(3):491-507.
 
-[2] Benjamini Y, Hochberg Y (2000): On the adaptive control of the false 
-discovery rate in multiple testing with independent statistics. Journal of 
+[2] Benjamini Y, Hochberg Y (2000): On the adaptive control of the false
+discovery rate in multiple testing with independent statistics. Journal of
 Educational and Behavioral Statistics 25:60-83.
 
-[3] Benjamini Y, Hochberg Y (1995): Controlling the false discovery rate: 
-A practical and powerful approach to multiple testing. Journal of Royal 
+[3] Benjamini Y, Hochberg Y (1995): Controlling the false discovery rate:
+A practical and powerful approach to multiple testing. Journal of Royal
 Statistical Society. Series B (Methodological): 57(1):289-300.
 
-[4] Reiss PT, Schwartzman A, Lu F, Huang L, Proal E (2012): Paradoxical 
+[4] Reiss PT, Schwartzman A, Lu F, Huang L, Proal E (2012): Paradoxical
 results of adaptive false discovery rate procedures in neuroimaging studies
 63(4):1833-1840.
 
@@ -34,17 +34,19 @@ Last modified 6th November 2017
 
 import numpy as np
 
+from scipy.interpolate import UnivariateSpline
+
 def lsu(pvals, q=0.05):
-    """The (non-adaptive) one-stage linear step-up procedure (LSU) for 
-    controlling the false discovery rate, i.e. the classic FDR method 
+    """The (non-adaptive) one-stage linear step-up procedure (LSU) for
+    controlling the false discovery rate, i.e. the classic FDR method
     proposed by Benjamini & Hochberg (1995).
-    
+
     Input arguments:
     pvals - P-values corresponding to a family of hypotheses.
     q     - The desired false discovery rate.
 
     Output arguments:
-    List of booleans indicating which p-values are significant (encoded 
+    List of booleans indicating which p-values are significant (encoded
     as boolean True values) and which are not (False values).
     """
     m = len(pvals)
@@ -56,7 +58,7 @@ def lsu(pvals, q=0.05):
     return significant
 
 def abh(pvals, q=0.05):
-    """The adaptive linear step-up procedure for controlling the false 
+    """The adaptive linear step-up procedure for controlling the false
     discovery rate.
 
     Input arguments:
@@ -100,7 +102,7 @@ def orc(pvals, m0, q=0.05):
         return lsu(pvals, q*m/m0)
 
 def tst(pvals, q=0.05):
-    """The two-stage linear step-up procedure (TST) for controlling the 
+    """The two-stage linear step-up procedure (TST) for controlling the
     false discovery rate.
 
     Input arguments:
@@ -125,9 +127,9 @@ def tst(pvals, q=0.05):
         return lsu(pvals, qstar)
 
 def paradoxical(pvals, q, significant):
-    """Validate results for paradoxical effects. Here, paradoxical refers to 
-    a case where an adaptive procedures rejects more null hypotheses than 
-    there would have been rejections if multiple testing was not accounted 
+    """Validate results for paradoxical effects. Here, paradoxical refers to
+    a case where an adaptive procedures rejects more null hypotheses than
+    there would have been rejections if multiple testing was not accounted
     for. See discussion by Reiss et al. in [4] for more information.
 
     Input arguments:
@@ -140,3 +142,52 @@ def paradoxical(pvals, q, significant):
     """The results are paradoxical if there are *more* significant p-values
     *after* the correction for multiple testing has been performed."""
     return np.sum(nocor_significant) < np.sum(significant)
+
+def qvalues(pvals, threshold=0.05, verbose=True):
+    """Function for estimating q-values from p-values using the
+    Storey-Tibshirani method [1].
+
+    Input arguments:
+    pvals       - P-values corresponding to a family of hypotheses.
+    threshold   - Threshold for deciding which q-values are significant.
+
+    Output arguments:
+    significant - An array of flags indicating which p-values are significant.
+    qvals       - Q-values corresponding to the p-values.
+    """
+
+    """Count the p-values. Find indices for sorting the p-values into
+    ascending order and for reversing the order back to original."""
+    m, pvals = len(pvals), np.asarray(pvals)
+    ind = np.argsort(pvals)
+    rev_ind = np.argsort(ind)
+    pvals = pvals[ind]
+
+    # Estimate proportion of features that are truly null.
+    kappa = np.arange(0, 0.96, 0.01)
+    pik = [sum(pvals > k) / (m*(1-k)) for k in kappa]
+    cs = UnivariateSpline(kappa, pik, k=3, s=None, ext=0)
+    pi0 = float(cs(1.))
+    print 'The estimated proportion of truly null features is %.3f' % pi0
+
+    # Sanity check
+    # TODO: check whether orig. paper has recommendations how to handle
+    if (pi0 < 0 or pi0 > 1):
+        pi0 = 1
+        if (verbose):
+            print 'The proportion was not in [0, 1] and was set as 1.'
+
+    # Compute the q-values.
+    qvals = np.zeros(np.shape(pvals))
+    qvals[-1] = pi0*pvals[-1]
+    for i in xrange(m-2, -1, -1):
+        qvals[i] = min(pi0*m*pvals[i]/float(i+1), qvals[i+1])
+
+    # Test which p-values are significant.
+    significant = np.zeros(np.shape(pvals), dtype='bool')
+    significant[ind] = qvals<threshold
+
+    """Order the q-values according to the original order of the p-values."""
+    qvals = qvals[rev_ind]
+    return significant, qvals
+
--- multipy/viz.py
+++ multipy/viz.py
@@ -15,6 +15,8 @@ WARNING: These functions have not been entirely validated yet.
 
 import matplotlib.pyplot as plt
 
+import numpy as np
+
 import seaborn as sb
 
 def plot_pval_hist(pvals, hist_bins=1e2):
@@ -38,4 +40,67 @@ def plot_pval_hist(pvals, hist_bins=1e2):
     ax1.set_xlabel('P-value')
     ax1.set_ylabel('Density')
     return fig
+
+def plot_qvalue_diagnostics(stats, pvals, qvals, show_plot=True):
+    """Visualize q-values similar to Storey and Tibshirani [1].
+
+    Input arguments:
+    stats     - Test statistics (e.g. Student's t-values)
+    pvals     - P-values corresponding to the test statistics
+    qvals     - Q-values corresponding to the p-values
+    show_plot - A flag for deciding whether the figure should be opened
+                immediately after being drawn.
+
+    Output arguments:
+    fig       - The drawn figure.
+    """
+    # Sort the p-values into ascending order for improved visualization.
+    stat_sort_ind, pval_sort_ind = np.argsort(stats), np.argsort(pvals)
+    thresholds = np.arange(0., 0.1, 0.001)
+    significant_tests = np.asarray([np.sum(qvals<t) for t in thresholds])
+
+    fig = plt.figure(figsize=(8, 6))
+    fig.subplots_adjust(top=0.95, bottom=0.1, left=0.09, right=0.96)
+
+    """Show q-values corresponding to the test statistics."""
+    ax1 = fig.add_subplot(221)
+    ax1.plot(stats[stat_sort_ind], qvals[stat_sort_ind])
+    ax1.set_xlabel('Test statistic')
+    ax1.set_ylabel('Q-value')
+
+    """Show q-values corresponding to the p-values."""
+    ax2 = fig.add_subplot(222)
+    ax2.plot(pvals[pval_sort_ind], qvals[pval_sort_ind])
+    ax2.set_xlabel('P-value')
+    ax2.set_ylabel('Q-value')
+
+    """Show the number of significant tests as a function of q-value
+    threshold."""
+    ax3 = fig.add_subplot(223)
+    ax3.plot(thresholds, significant_tests)
+    ax3.set_xlabel('Q-value')
+    ax3.set_ylabel('Number of significant tests')
+
+    """Show the number of expected false positives as a function of the
+    number of significant tests."""
+    ax4 = fig.add_subplot(224)
+    ax4.plot(significant_tests, thresholds*significant_tests)
+    ax4.set_xlabel('Number of significant tests')
+    ax4.set_ylabel('Number of expected false positives')
+
+    """Return the drawn figure."""
+    if (show_plot):
+        plt.show() # Open the figure.
+    return fig
+
+def plot_qvalue_pi0_fit(kappa, pik, cs):
+    """Make a diagnostic plot of the estimate of pi0."""
+    fig = plt.figure(facecolor='white', edgecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(kappa, pik, '.', markersize=10)
+    ax.plot(np.linspace(0, 1, 100), cs(np.linspace(0, 1, 100)),
+            '-', markersize=10)
+    ax.set_xlabel('$\lambda$')
+    ax.set_ylabel('$\pi_{0}(\lambda)$')
+    plt.show()
 
