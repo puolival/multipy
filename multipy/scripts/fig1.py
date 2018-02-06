e542d96d4b723b2a09acc386aed4e11fe37b9c3b
puolival <tuomas.puolivali@helsinki.fi>
2018-02-06 15:39:23 +0200

    Show data for different critical levels.

--- multipy/scripts/fig1.py
+++ multipy/scripts/fig1.py
@@ -4,21 +4,26 @@ import numpy as np
 
 import seaborn as sns
 
-n_tests = np.arange(1, 40)
+n_tests = np.arange(1, 200)
 
 """Compute Pr(at least one false positive) in m tests."""
-alpha = 0.05
-pr_1fp = 1 - (1 - alpha) ** n_tests
+alpha = np.asarray([0.05, 0.01, 0.001])
+n_alpha = len(alpha)
+
+pr_1fp = np.zeros([n_alpha, len(n_tests)])
+for i, a in enumerate(alpha):
+    pr_1fp[i, :] = 1 - (1 - a) ** n_tests
 
 """Plot the data."""
 fig = plt.figure(figsize=(6, 4))
 fig.subplots_adjust(top=0.92, bottom=0.13, left=0.12, right=0.95)
 
 ax = fig.add_subplot(111)
-ax.plot(n_tests, pr_1fp)
-ax.set_xlabel('Number of comparisons')
-ax.set_ylabel('$\mathbb{P}($at least one false positive$)$')
-ax.set_ylim([0, 1])
-ax.set_xlim([1, n_tests[-1]])
+ax.plot(n_tests, pr_1fp.T)
+ax.set_xlabel('Number of tests')
+ax.set_ylabel('$\mathbb{P}$ (at least one false positive$)$')
+ax.set_ylim([0-0.04, 1+0.04])
+ax.set_xlim([n_tests[0]-5, n_tests[-1]+5])
+ax.legend(alpha)
 
 plt.show()
