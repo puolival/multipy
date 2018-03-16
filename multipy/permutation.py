b0e97dc337262e96694a715e9aba27cd425cedfd
Puoliväli Tuomas B <puolival@dx7-h916-01.biocenter.helsinki.fi>
2018-03-16 11:04:16 +0200

    Changed part of the local variables to input arguments. Added comments. Still not functional.

--- multipy/permutation.py
+++ multipy/permutation.py
@@ -46,7 +46,16 @@ def _cluster_by_adjacency(sel_samples):
     return clusters
 
 def _cluster_stat(tstat, clusters):
-    """Function for computing the cluster mass statistic."""
+    """Function for computing the cluster mass statistic.
+
+    Input arguments:
+    tstat    - T-statistic for each variable.
+    clusters - A vector of cluster numbers grouping the t-statistics as
+               returned by the function _cluster_by_adjacency.
+
+    Output arguments:
+    cstat    - The cluster mass statistic.
+    """
     n_clusters = np.max(clusters)
     if (n_clusters == 0):
         return np.nan
@@ -55,20 +64,39 @@ def _cluster_stat(tstat, clusters):
                                               arange(1, n_clusters+1)]
         return np.max(cstat)
 
-def permutation_test(X, Y):
-    """Find the number of samples and variables in the two sets of data."""
-    (n_samples_x, n_vars_x), (n_samples_y, n_vars_y) = np.shape(X), np.shape(Y)
+def permutation_test(X, Y, n_permutations=1000, threshold=1, tail='both'):
+    """Permutation test for a significant difference between two sets of data.
+
+    Input arguments:
+    X              - An N_1-by-m matrix (NumPy ndarray) where N is the number
+                     of samples and m is the number of variables. The first
+                     group of data.
+    Y              - An N_2-by-m matrix. The second group of data.
+    n_permutations - Number of permutations. Consider how accurate p-value
+                     is needed.
+    threshold      -
+    tail           - Only two-sided tests are supported for the moment (i.e.
+                     tail = 'both').
+
+    Output arguments:
+    pval           - Permutation test p-value.
+    """
+
+    """Find the number of samples in each of the two sets of data."""
+    (n_samples_x, _), (n_samples_y, _) = np.shape(X), np.shape(Y)
     n_total = n_samples_x + n_samples_y
 
-    threshold = 1
-    n_permutations = 1000
-
+    """Do an independent two-sample t-test for each variable using the
+    original (unpermuted) data. Compute the cluster mass statistic that is
+    compared to the permutation distribution."""
     ref_tstat, _ = ttest_ind(X, Y)
-    ref_cstat = _cluster_stat(ref_tstat,
-                              _cluster_by_adjacency(ref_tstat > threshold))
+    ref_clusters = _cluster_by_adjacency(ref_tstat > threshold)
+    ref_cstat = _cluster_stat(ref_tstat, ref_clusters)
 
     """Combine the two sets of data."""
     Z = np.vstack([X, Y])
+
+    """Do the permutations."""
     cmax = np.zeros(n_permutations)
 
     for p in arange(0, n_permutations):
@@ -86,8 +114,10 @@ def permutation_test(X, Y):
     cmax = cmax[~np.isnan(cmax)]
     pval = np.sum(cmax > ref_cstat) / n_permutations
 
+    return pval
+
 """Generate some test data."""
 X = normal(loc=2, scale=1, size=(20, 300))
 Y = normal(loc=0, scale=1, size=(20, 300))
 
-permutation_test(X, Y)
+print permutation_test(X, Y)
