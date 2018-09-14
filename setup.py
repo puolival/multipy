99e3dd19791c2996ae3a243fc63c45db85ae9596
Puoliväli Tuomas B <puolival@dx7-h916-01.biocenter.helsinki.fi>
2018-09-14 13:06:13 +0300

    Updated setup files.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='multipy',
-      version='0.12',
+      version='0.13',
       description='Multiple hypothesis testing in Python',
       url='https://puolival.github.io/multipy/',
       author='Tuomas Puoliväli',
