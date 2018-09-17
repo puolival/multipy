74c94f9466a51c9fd64fb653dd99addb3535dbd8
Puoliväli Tuomas B <puolival@dx7-h916-01.biocenter.helsinki.fi>
2018-09-17 10:42:43 +0300

    Assume equal population variances. Added ref to Bennett et al.

--- multipy/compare.py
+++ multipy/compare.py
@@ -6,12 +6,18 @@ Author: Tuomas Puoliväli
 Email: tuomas.puolivali@helsinki.fi
 Last modified: 11th September 2018
 License: Revised 3-clause BSD
+
+References:
+
+[1] Bennett CM, Wolford GL, Miller MB (2009): The principled control of
+    false positives in neuroimaging. Social Cognitive and Affective
+    Neuroscience 4(4):417-422.
 """
 
 from data import square_grid_model
 
 from fdr import lsu, qvalue, tst
-from fwer import sidak
+from fwer import sidak, holm_bonferroni, hochberg
 from rft import rft_2d
 from permutation import tfr_permutation_test
 
@@ -20,7 +26,8 @@ from viz import plot_grid_model
 """Generate the test data."""
 nl, sl = 90, 30
 N, delta = 25, 0.7
-X, X_tstats, X_raw, Y_raw = square_grid_model(nl, sl, N, delta)
+X, X_tstats, X_raw, Y_raw = square_grid_model(nl, sl, N, delta,
+                                              equal_var=True)
 alpha = 0.05
 
 """Apply each correction technique to the generated dataset."""
@@ -40,7 +47,13 @@ Y_tst = tst(X.flatten(), q=alpha)
 Y_tst = Y_tst.reshape(nl, nl)
 
 Y_permutation = tfr_permutation_test(X_raw, Y_raw, n_permutations=100,
-                                     alpha=0.05, threshold=1)
+                                     alpha=alpha, threshold=1)
+
+Y_holm = holm_bonferroni(X.flatten(), alpha=alpha)
+Y_holm = Y_holm.reshape(nl, nl)
+
+Y_hochberg = hochberg(X.flatten(), alpha=alpha)
+Y_hochberg = Y_hochberg.reshape(nl, nl)
 
 """Visualize the results."""
 fig_nocor = plot_grid_model(X<alpha, nl, sl)
@@ -63,6 +76,12 @@ fig_tst.axes[0].set_title('Two-stage procedure')
 
 fig_permutation = plot_grid_model(Y_permutation, nl, sl)
 fig_permutation.axes[0].set_title('Permutation')
+
+fig_holm = plot_grid_model(Y_holm, nl, sl)
+fig_holm.axes[0].set_title('Holm-Bonferroni')
+
+fig_hochberg = plot_grid_model(Y_hochberg, nl, sl)
+fig_hochberg.axes[0].set_title('Hochberg')
 
 import matplotlib.pyplot as plt
 plt.show()
