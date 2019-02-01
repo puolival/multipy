fca9106059aed3211aaa8323dd0cf64a5916c8c7
Puoliväli Tuomas B <puolival@dx7-h916-01.biocenter.helsinki.fi>
2019-02-01 10:03:03 +0200

    Initial implementation of the fwer replicability method.

--- multipy/repeat.py
+++ multipy/repeat.py
@@ -17,9 +17,14 @@ References:
     a primary study of high dimension to a follow-up study. Journal of the
     American Statistical Association 108(504):1480-1492.
 
+NOTE: Work in progress. Untested code.
+
 """
 
-def fwer_replicability(pvals_primary, pvals_followup, emph_primary, method):
+import numpy as np
+
+def fwer_replicability(pvals_primary, pvals_followup, emph_primary, method,
+                       alpha=0.05):
     """The Bogomolov & Heller family-wise error rate (FWER) replicability
     method.
 
@@ -37,11 +42,40 @@ def fwer_replicability(pvals_primary, pvals_followup, emph_primary, method):
         Emphasis given to the primary study.
 
     method : function
-        The method used to control FWER.
-    """
-    pass
+        The method used to control for false positives.
 
-def partial_conjuction(pvals_primary, pvals_followup, method):
+    alpha : float
+        The critical level. Defaults to 0.05 when unspecified.
+
+    Output arguments:
+    =================
+    replicable : ndarray [n_tests, ]
+        Array of booleans indicating which tests can be considered to be
+        reproducible.
+    """
+
+    """Compute emphasis given to the follow-up study."""
+    if ((emph_primary < 0) or (emph_primary > 1)):
+        raise Exception('Emphasis given to the primary study must be' +
+                        ' in (0, 1)!')
+    emph_followup = 1-emph_primary
+
+    """Apply the correction to both sets of p-values."""
+    fwer_methods = ['bonferroni', 'hochberg', 'holm_bonferroni', 'sidak']
+    if (method.__name__ in fwer_methods):
+        significant_primary = method(pvals_primary, emph_primary*alpha)
+        # Exclude not-a-number values.
+        ind = np.isnan(pvals_followup) == False
+        significant_followup = method(pvals_followup[ind],
+                                      emph_followup*alpha)
+
+    """Decide which tests are replicable."""
+    n_tests = len(pvals_primary)
+    replicable = np.zeros(n_tests, dtype='bool')
+    replicable[ind] = significant_primary[ind] & significant_followup
+    return replicable
+
+def partial_conjuction(pvals_primary, pvals_followup, method, alpha=0.05):
     """The partial conjuction method for deciding reproducible effects.
 
     Input arguments:
@@ -56,5 +90,14 @@ def partial_conjuction(pvals_primary, pvals_followup, method):
 
     method : function
         The method used to control FWER.
+
+    alpha : float
+        The critical level. Defaults to 0.05 when unspecified.
+
+    Output arguments:
+    =================
+    reproducible : ndarray [n_tests, ]
+        Array of booleans indicating which tests can be considered to be
+        reproducible.
     """
     pass
