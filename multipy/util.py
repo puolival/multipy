67460d5f27f836d0e3ef899ad002299c4756bb77
Puoliväli Tuomas B <puolival@dx7-h916-01.biocenter.helsinki.fi>
2019-04-09 14:29:15 +0300

    Compute the number of true and false positives and negatives in the spatial separate-classes model.

--- multipy/util.py
+++ multipy/util.py
@@ -69,6 +69,42 @@ def grid_model_counts(Y, nl, sl):
     fp, tn = np.sum(Y[mask] == True), np.sum(Y[mask] == False)
     return tp, fp, tn, fn
 
+def separate_classes_model_counts(Y, nl, sl):
+    """Function for counting the number of true and false positives and
+    true and false negatives in the spatial separate-classes model.
+
+    Input arguments:
+    ================
+    Y : ndarray
+        Array of booleans indicating which tests were declared significant.
+    nl : int
+        The side length of the noise region for a single class. The size of
+        the matrix Y should be [nl, 2*nl].
+    sl : int
+        The side length of a signal region.
+
+    Output arguments:
+    =================
+    tp, fp, tn, fn : int
+        The number of true positives (tp), false positives (fp),
+        true negatives (tn), and false negatives (fn).
+    """
+    Y = np.asarray(Y, dtype='bool')
+    d = (nl-sl) // 2
+
+    """Process data within the signal regions."""
+    tp = (np.sum(Y[d:(nl-d), d:(nl-d)] == True) +
+          np.sum(Y[d:(nl-d), (nl+d):(2*nl-d)] == True))
+    fn = (np.sum(Y[d:(nl-d), d:(nl-d)] == False) +
+          np.sum(Y[d:(nl-d), (nl+d):(2*nl-d)] == False))
+
+    """Process data within the noise region."""
+    mask = np.ones(np.shape(Y), dtype='bool')
+    mask[d:(nl-d), d:(nl-d)] = False
+    mask[d:(nl-d), (nl+d):(2*nl-d)] = False
+    fp, tn = np.sum(Y[mask] == True), np.sum(Y[mask] == False)
+    return tp, fp, tn, fn
+
 def roc(counts):
     """Function computing TPR and FPR.
 
@@ -100,11 +136,11 @@ def empirical_power(n_tp, n_at):
     """
 
     """Compute the empirical power. Check that the result is in [0, 1]."""
-    epwr = float(n_tp) / float(n_at)
-    if ((epwr > 1) | (epwr < 0)):
+    pwr = float(n_tp) / float(n_at)
+    if ((pwr > 1) | (pwr < 0)):
         raise Exception('Invalid input parameters!')
 
-    return epwr
+    return pwr
 
 def empirical_fpr(n_fp, n_nt):
     """Compute empirical false positive rate (FPR).
