d5a8e76966d264eb43b471058d395aec673df943
Puoliväli Tuomas B <puolival@dx7-h916-01.biocenter.helsinki.fi>
2019-04-12 16:56:28 +0300

    Small changes based on re-reading the original authors' implementation. Improved documentation.

--- multipy/rvalue.py
+++ multipy/rvalue.py
@@ -4,6 +4,7 @@
 Author: Tuomas Puoliväli
 Email: tuomas.puolivali@helsinki.fi
 License: Revised 3-clause BSD
+Last modified: 12th April 2019
 
 References:
 
@@ -11,17 +12,14 @@ References:
 studies have replicated findings in a preliminary large-scale omics study.
 The Proceedings of the National Academy of Sciences of the United States
 of America 111(46):16262-16267.
-
 """
 
 import numpy as np
 
-import pandas as ps
-
 from scipy.optimize import root
 from scipy.stats import rankdata
 
-def _r_value_f(x, m, p1, p2, c2=0.5, l00=0.8):
+def _fdr_rvalue_f(x, m, p1, p2, c2=0.5, l00=0.8):
     """Function for computing the FDR r-value of a given feature.
 
     Input arguments:
@@ -49,37 +47,46 @@ def _r_value_f(x, m, p1, p2, c2=0.5, l00=0.8):
     r = np.min([r, np.ones(np.shape(r))], axis=0)
     return r
 
-def _f(x, i, m, p1, p2, c2, l00):
-    # Find f(x) = x
-    return _r_value_f(x, m, p1, p2, c2, l00)[i] - x
+def _fdr_rvalue_f_aux(x, i, m, p1, p2, c2, l00):
+    """Auxiliary function used to enable solving f(x) = x."""
+    return _fdr_rvalue_f(x, m, p1, p2, c2, l00)[i] - x
 
 def fdr_rvalue(p1, p2, m, c2=0.5, l00=0.8):
-    """Function for computing reproducibility values (r-values) using the
-    method suggested by Heller et al.
+    """Function for computing FDR r-values using the method suggested by
+    Heller et al.
 
     Input arguments:
     ================
-    R : list of tuples
-      Primary and follow-up study P-values.
+    p1, p2 : ndarray [n_tests, ]
+        The p-values that were selected for follow-up from the primary
+        study (p1) and the corresponding p-values observed in the
+        follow-up study (p2).
 
-    m_primary : int
-      Number of features examined in the primary study.
+    m : int
+        The number of tests considered in the primary study.
 
-    emphasis: float
-      Emphasis given to the follow-up study. Must be a value on the open
-      interval (0, 1).
+    c2 : float in range (0, 1)
+        The emphasis given to the follow-up study.
+
+    l00 : float in range [0, 1)
+        The lower bound on f00, which is "the fraction of features, of the
+        m features examined in the primary study, that are null in both
+        studies".
     """
-    m = 5
-    c2 = 0.5
-    l00 = 0.8
-    rv = np.zeros(np.shape(p2))
+
+    # TODO: Consider implementing the other variations of computing c1.
 
     tol = np.min([np.min([p1, p2]), 0.0001])
 
+    """Compute the r-values."""
+    rvalue = np.zeros(np.shape(p2), dtype='float')
     for i, _ in enumerate(p2):
-        sol = root(_f, x0=0.5, args=(i, m, p1, p2, c2, l00), tol=tol)['x']
-        rv[i] = sol
-    return rv
+        if (_f(1, i, m, p1, p2, c2, l00) >= 0):
+            rvalue[i] = 1
+        else:
+            sol = root(_fdr_rvalue_f_aux, x0=0.5,
+                       args=(i, m, p1, p2, c2, l00), tol=tol)['x']
+            rvalue[i] = sol
 
-# df = ps.read_csv('/home/puolival/multipy_data/CrohnExample.csv')
+    return rvalue
 
