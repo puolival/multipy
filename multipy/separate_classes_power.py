8e120f0170149a0e959f40c862b7a51249bb85c3
Puoliväli Tuomas B <puolival@dx7-h916-01.biocenter.helsinki.fi>
2019-04-25 14:16:36 +0300

    Added a parameter for deciding whether to perform a single combined analysis or two separate analyses.

--- multipy/separate_classes_power.py
+++ multipy/separate_classes_power.py
@@ -31,7 +31,8 @@ from util import (empirical_power, separate_classes_model_counts,
 from viz import plot_separate_classes_model
 
 def separate_classes_model_power(deltas, n_iter=20, alpha=0.05, nl=45,
-                                 sl=15, method=lsu):
+                                 sl=15, method=lsu,
+                                 single_analysis=False):
     """Function for simulating data under the spatial separate-classes
     model at various effect sizes.
 
@@ -53,6 +54,10 @@ def separate_classes_model_power(deltas, n_iter=20, alpha=0.05, nl=45,
 
     method : function
         The applied correction method.
+
+    single_analysis : bool
+        A flag for deciding whether to perform a single combined analysis
+        or two separate analyses.
     """
     # TODO: make a proper function
     n_deltas = len(deltas)
@@ -61,8 +66,23 @@ def separate_classes_model_power(deltas, n_iter=20, alpha=0.05, nl=45,
     for ind in np.ndindex(n_deltas, n_deltas, n_iter):
         delta1, delta2 = deltas[ind[0]], deltas[ind[1]]
         X = spatial_separate_classes_model(delta1, delta2)[0]
-        Y = method(X.flatten(), alpha)
-        Y = Y.reshape([nl, 2*nl])
+
+        """Perform the multiple testing using either a single combined
+        analysis or two separate analyses. If two analyses are performed,
+        it is assumed that the separate classes are known a priori by
+        the experiments. (However, it would be interesting to also test
+        happens when this assumption is incorrect.)
+        """
+        if (single_analysis):
+            Y = method(X.flatten(), alpha)
+            Y = Y.reshape([nl, 2*nl])
+        else:
+            Y1, Y2 = (method(X[:, 0:nl].flatten(), alpha),
+                      method(X[:, nl:].flatten(), alpha))
+            Y1, Y2 = Y1.reshape([nl, nl]), Y2.reshape([nl, nl])
+            Y = np.hstack([Y1, Y2])
+
+        """Compute empirical power."""
         tp, _, _, fn = separate_classes_model_counts(Y, nl, sl)
         pwr[ind] = empirical_power(tp, tp+fn)
 
@@ -88,7 +108,7 @@ def plot_separate_classes_model_power(effect_sizes, pwr):
     sns.set_style('darkgrid')
     fig = plt.figure(figsize=(8, 5))
     ax = fig.add_subplot(111)
-    ax.imshow(pwr, origin='lower', cmap='viridis', interpolation='none')
+    im = ax.imshow(pwr, origin='lower', cmap='viridis', interpolation='none')
     ax.grid(False)
 
     # Only display every other <x/y>tick.
@@ -100,13 +120,22 @@ def plot_separate_classes_model_power(effect_sizes, pwr):
 
     ax.set_xlabel('Effect size $\Delta_1$')
     ax.set_ylabel('Effect size $\Delta_2$')
-    fig.tight_layout()
-    return fig
+    return fig, im
 
 def simulate_separate_classes_model():
+    """Compute empirical power at the chosen effect sizes using the chosen
+    multiple testing method."""
+    single_analysis = True
     effect_sizes = np.linspace(0.2, 2.4, 12)
-    pwr = separate_classes_model_power(effect_sizes)
-    fig = plot_separate_classes_model_power(effect_sizes, pwr,
-                                            method=lsu)
+    method = lsu
+    pwr = separate_classes_model_power(effect_sizes, method=method,
+                                       single_analysis=single_analysis)
+
+    """Visualize the results."""
+    fig, im = plot_separate_classes_model_power(effect_sizes, pwr)
+    fig.axes[0].set_title('Method: %s' % method.__name__)
+    fig.colorbar(im)
+    fig.tight_layout()
+    fig.axes[0].grid(False)
     plt.show()
 
